337cfe02ed02691331f7fa287d476aa6f15c947f
AlvaroCubi <55387701+AlvaroCubi@users.noreply.github.com>
2024-12-03 15:11:38 +0100

    Finish development

--- src/csv_based_workflow/homogenize_components.py
+++ src/csv_based_workflow/homogenize_components.py
@@ -67,6 +67,9 @@ def prepare_component_row(csv_dict, material_mix_by_mass, total_volume, componen
     component_row["MATERIAL"] = "Mix{}".format(new_id)
     component_row["DENSITY [g/cm3]"] = sum(material_mix_by_mass.values()) / total_volume
 
+    max_cell_id = get_max_cell_id(csv_dict)
+    component_row["CELL IDs"] = "[{}, {}]".format(max_cell_id + 1, max_cell_id + 1)
+
     comment = "Homogenized from components: {}".format(", ".join(component_ids)) + ". "
     comment += "Material mix by mass [g]: "
     for material, mass in material_mix_by_mass.items():
@@ -74,6 +77,15 @@ def prepare_component_row(csv_dict, material_mix_by_mass, total_volume, componen
     comment = comment[:-2] + "."
     component_row["COMMENT"] = comment
     return component_row
+
+
+def get_max_cell_id(csv_dict):
+    max_cell_id = 0
+    for row in csv_dict.values():
+        cell_ids = re.findall(r"\d+", row["CELL IDs"])
+        for cell_id in cell_ids:
+            max_cell_id = max(int(cell_id), max_cell_id)
+    return max_cell_id
 
 
 def write_new_component_to_csv(component_row):
@@ -86,8 +98,83 @@ def write_new_component_to_csv(component_row):
         writer.writerow(component_row)
 
 
-def create_new_component(new_component_id):
-    pass
+def create_new_component(name, total_volume):
+    center = MeasureHelper.GetCenterOfMass(Selection.GetActive())
+    comp = ComponentHelper.CreateAtRoot(name)
+    ComponentHelper.SetActive(comp)
+    body = create_prism(comp)
+    move_body_to_point(body, center)
+    adjust_volume(body, total_volume)
+    set_components_as_blue_transparent([comp])
+    ComponentHelper.SetRootActive(None)
+
+
+def create_prism(comp):
+    # Set Sketch Plane
+    sectionPlane = Plane.PlaneXY
+    ViewHelper.SetSketchPlane(sectionPlane, None)
+
+    # Sketch Rectangle
+    point1 = Point2D.Create(CM(0), CM(0))
+    point2 = Point2D.Create(CM(0), CM(10))
+    point3 = Point2D.Create(CM(10), CM(10))
+    SketchRectangle.Create(point1, point2, point3)
+
+    # Solidify Sketch
+    mode = InteractionMode.Solid
+    ViewHelper.SetViewMode(mode, None)
+
+    # Extrude 1 Face
+    selection = FaceSelection.Create(comp.GetBodies()[0].Faces[0])
+    options = ExtrudeFaceOptions()
+    options.ExtrudeType = ExtrudeType.ForceIndependent
+    ExtrudeFaces.Execute(selection, CM(10), options)
+
+    return comp.GetBodies()[0]
+
+
+def move_body_to_point(body, point):
+    selection = BodySelection.Create(body)
+    vector = Vector.Create(point.X - CM(5), point.Y - CM(5), point.Z - CM(5))
+    options = MoveOptions()
+    Move.Translate(selection, vector, options)
+
+
+def adjust_volume(body, target_volume):
+    step = 1  # in mm
+    max_diff = 1  # in %
+    options = OffsetFaceOptions()
+    options.OffsetMode = OffsetMode.MoveFacesTogether
+
+    sel = FaceSelection.Create(body.Faces)
+    current_volume = get_body_volume(body)
+    dif = (target_volume - current_volume) / target_volume * 100
+    positive_sense = True if dif > 0 else False
+    while abs(dif) > max_diff:
+        if dif > 0:
+            if not positive_sense:
+                step = step * 0.5
+                positive_sense = True
+            OffsetFaces.Execute(sel, MM(step), options)
+        else:
+            if positive_sense:
+                step = step * 0.5
+                positive_sense = False
+            OffsetFaces.Execute(sel, MM(-step), options)
+        current_volume = get_body_volume(body)
+        dif = (target_volume - current_volume) / target_volume * 100
+
+
+def get_body_volume(body):
+    return body.MassProperties.Mass * 1000000  # cm3
+
+
+def set_components_as_blue_transparent(comps):
+    sel = Selection.Create(comps)
+    options = SetColorOptions()
+    options.FaceColorTarget = FaceColorTarget.Body
+    ColorHelper.SetColor(sel, options, ColorHelper.Blue)
+    ColorHelper.SetFillStyle(sel, FillStyle.Transparent)
 
 
 def main():
@@ -111,7 +198,7 @@ def main():
         csv_dict, material_mix_by_mass, total_volume, component_ids
     )
     write_new_component_to_csv(component_row)
-    create_new_component(component_row["Component ID"])
+    create_new_component(component_row["Component ID"], total_volume)
 
 
 main()
